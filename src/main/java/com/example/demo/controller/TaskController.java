c777d84ad1cd673c270a59be86a1dc815ba2632c
Tigran Mkrtchyan <tigran@itspace.am>
2019-05-14 14:25:44 +0400

    update project

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 @RestController
 public class TaskController {
