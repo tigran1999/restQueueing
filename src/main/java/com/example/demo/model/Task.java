cc9602b6bc58b900077a8d367b1533d46ab67686
Tigran Mkrtchyan <tigran@itspace.am>
2019-05-24 10:04:32 +0400

    Merge branch 'master' of C:\Users\developer\Desktop\projects\restQueueing with conflicts.

--- src/main/java/com/example/demo/model/Task.java
+++ src/main/java/com/example/demo/model/Task.java
@@ -1,30 +1,41 @@
 package com.example.demo.model;
 
+import com.example.demo.model.enums.TaskType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table
 @Builder
 public class Task {
 
     @Id
     @GeneratedValue
-    private int id;
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID id = UUID.randomUUID();
 
+    @Column(name = "file_url")
+    private String fileUrl;
+
+    @Column(name = "created_date")
     private Date createdDate;
 
+    @Column(name = "finishedDate")
     private Date finishedDate;
 
+    @Column
     private boolean downloaded;
-
-    private String fileUrl;
 
 }
