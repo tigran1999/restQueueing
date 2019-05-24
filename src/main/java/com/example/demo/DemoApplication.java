d78d033bf5f5ea788379555c36293649155d89a6
Tigran Mkrtchyan <tigran@itspace.am>
2019-05-24 10:09:47 +0400

    update project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,10 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class DemoApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 
 }
